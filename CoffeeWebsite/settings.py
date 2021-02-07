8c27986eca8818890b5e86112e5d5a16eb94d310
slleekk <Ayomidedans@gmail.com>
2021-02-07 22:57:26 +0100

    little changes

--- CoffeeWebsite/settings.py
+++ CoffeeWebsite/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 import django_heroku
+from .secret_settings import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,8 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wkk)h$!_xnc)p-eaqhm4#o-hn3rq^dxwh&fdzeqv^1*sw5$fp1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
