b9302303ce74a7763fd156f4e2a4f56d4852a9e6
slleekk <Ayomidedans@gmail.com>
2021-02-08 16:02:15 +0100

    little changes

--- coffeeapp/models.py
+++ coffeeapp/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 class Contact(models.Model):
     name = models.CharField(max_length=80)
-    email = models.EmailField()
-    message = models.TextField()
+    email = models.EmailField(blank=True,)
+    message = models.TextField(blank=True)
