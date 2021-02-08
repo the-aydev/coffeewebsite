2d67734a8896f146ebe32c6f0a6f474e5d5957f9
slleekk <Ayomidedans@gmail.com>
2021-02-08 16:05:59 +0100

    little changes

---
+++ CoffeeWebsite/sec.py
@@ -0,0 +1,2 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'wkk)h$!_xnc)p-eaqhm4#o-hn3rq^dxwh&fdzeqv^1*sw5$fp1'
--- CoffeeWebsite/settings.py
+++ CoffeeWebsite/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 import django_heroku
-from .secret_settings import *
+from .sec import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
