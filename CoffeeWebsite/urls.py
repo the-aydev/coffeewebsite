87985fce5387b97440793b18bbcc897027208299
slleekk <Ayomidedans@gmail.com>
2021-02-09 04:53:19 +0100

    Procfile

--- CoffeeWebsite/urls.py
+++ CoffeeWebsite/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('coffeeapp/', include('coffeeapp.urls')),
+    path('', include('coffeeapp.urls')),
 ]
