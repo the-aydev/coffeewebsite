0df9270856ac26d603efb4fb0041df2043c6596e
slleekk <Ayomidedans@gmail.com>
2021-02-25 01:10:29 +0100

    tweaked app for heroku

--- CoffeeWebsite/settings.py
+++ CoffeeWebsite/settings.py
@@ -14,6 +14,8 @@ import sys
 import os
 from pathlib import Path
 import django_heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,6 +82,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'CoffeeWebsite.urls'
@@ -156,5 +159,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.dirname(BASE_DIR) + '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATICFILES_STORAGE = 'whitenoisse.storage.CompressedManifestStaticFilesStorage'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 django_heroku.settings(locals())
--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python CoffeeWebsite/manage.py runserver 0.0.0.0:$PORT
+web: gunicorn CoffeeWebsite.wsgi
